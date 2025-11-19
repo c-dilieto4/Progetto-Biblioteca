cd8bcf79d1c5187fc8ee1285c3679a4d228ad190
gerry <gerry@host.docker.internal>
2025-11-19 21:23:54 +0100

    modifica di Gerardo

--- Biblioteca/src/main/java/Main.java
+++ Biblioteca/src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("CHECK");
-        System.out.println("Check di Lorenzo");
+        System.out.println("Check di Gerardo");
     }
     
 }
