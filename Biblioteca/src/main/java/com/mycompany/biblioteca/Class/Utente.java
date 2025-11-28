2edbdfa20992575b22076dfeba5cc05bebd04073
alessandro <alessandro@mac-97329b.station>
2025-11-28 16:25:33 +0100

    ale commit

---
+++ Biblioteca/src/main/java/com/mycompany/biblioteca/Class/Utente.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.biblioteca.Class;
+
+/**
+ *
+ * @author alessandro
+ */
+public class Utente {
+    
+    private final String matricola;
+    private String nome;
+    private String cognome;
+    private String email;
+    private .... prestitiAttivi;
+    
+    public Utente(String matricola, String nome, String cognome, String email, .... prestitiAttivi){
+        
+        this.matricola=matricola;
+        this.nome=nome;
+        this.cognome=cognome;
+        this.email=email;
+        this.prestitiAttivi=prestitiAttivi;
+        
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public ... getPrestitiAttivi(){
+        return;
+    }
+}
