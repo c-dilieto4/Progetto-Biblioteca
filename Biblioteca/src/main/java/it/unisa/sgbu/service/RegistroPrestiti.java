8932bddccc409500c514d2c6db6b55d0e87dda24
alessandro <alessandro@172.19.142.188>
2025-12-03 12:01:38 +0100

    Update class Anagrafica

--- Biblioteca/src/main/java/it/unisa/sgbu/service/RegistroPrestiti.java
+++ Biblioteca/src/main/java/it/unisa/sgbu/service/RegistroPrestiti.java
@@ -5,10 +5,39 @@
  */
 package it.unisa.sgbu.service;
 
+import it.unisa.sgbu.domain.*;
+import it.unisa.sgbu.io.*;
+import java.util.*;
+import java.time.*;
+
 /**
  *
  * @author dilie
  */
 public class RegistroPrestiti {
+    
+    private List<Prestito> prestitiAttivi;
+    private final int LimitePrestiti=3;
+    private Catalogo catologo;
+    private Anagrafica anagrafica;
+    private ILogger logger;
+    
+    public RegistroPrestiti(Catalogo catologo, Anagrafica anagrafica, ILogger logger){
+    }
+    
+    public Prestito registraPrestito(String isbn, LocalDate dataPrev){
+    }
+    
+    public boolean registraRestituzione(String idPrestito){
+    }
+    
+    public List<Prestito> getPrestitiAttivi(){
+    }
+    
+    public List<Prestito> getPrestitiInRitardo(){
+    }
+    
+    public boolean haPrestitiAttivi(String matricola){
+    }
     
 }
