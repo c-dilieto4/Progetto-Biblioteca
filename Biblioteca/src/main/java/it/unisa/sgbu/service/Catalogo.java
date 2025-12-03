2fc21adc59d0cc381e410e891d3db73852e3efc4
alessandro <alessandro@172.19.142.188>
2025-12-03 11:46:04 +0100

    Update class Catalogo

--- Biblioteca/src/main/java/it/unisa/sgbu/service/Catalogo.java
+++ Biblioteca/src/main/java/it/unisa/sgbu/service/Catalogo.java
@@ -5,10 +5,39 @@
  */
 package it.unisa.sgbu.service;
 
+import it.unisa.sgbu.domain.Libro;
+import it.unisa.sgbu.io.*;
+import java.util.*;
+
 /**
  *
  * @author dilie
  */
 public class Catalogo {
+    
+    private Map<String, Libro> collezione;
+    private IArchivioDati archivio;
+    private ILogger logger;
+    
+    public Catalogo(IArchivioDati archivio, ILogger logger){
+    }
+    
+    public boolean aggiungiLibro(Libro l){
+    }
+    
+    public boolean rimuoviLibro(String isbn){
+    }
+    
+    public boolean modificaLibro(String isbn, Libro nl){
+    }
+    
+    public Libro getLibro(String isbn){
+    }
+    
+    public List<Libro> ricerca(String query, String campo){
+    }
+    
+    public List<Libro> visualizzaOrdinata(){
+    }
     
 }
