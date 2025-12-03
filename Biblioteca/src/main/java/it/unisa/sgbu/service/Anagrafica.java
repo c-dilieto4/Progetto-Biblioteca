f14945998e66b8170a52c72e2c38dd69c4d5f430
alessandro <alessandro@172.19.142.188>
2025-12-03 11:52:54 +0100

    Update class Anagrafica

--- Biblioteca/src/main/java/it/unisa/sgbu/service/Anagrafica.java
+++ Biblioteca/src/main/java/it/unisa/sgbu/service/Anagrafica.java
@@ -5,10 +5,39 @@
  */
 package it.unisa.sgbu.service;
 
+
+import it.unisa.sgbu.domain.*;
+import it.unisa.sgbu.io.*;
+import java.util.*;
+
 /**
  *
  * @author dilie
  */
 public class Anagrafica {
     
+    private Map<String, Utente> registroUtenti;
+    private IArchivioDati archivio;
+    private ILogger logger;
+    
+    public Anagrafica(IArchivioDati archivio, ILogger logger){
+    }
+    
+    public boolean aggiungiUtente(Utente u){
+    }
+    
+    public boolean rimuoviUtente(String matiricola){
+    }
+    
+    public boolean modificaUtente(String matiricola, Utente u){
+    }
+    
+    public Utente getUtente(String matiricola){
+    }
+    
+    public List<Utente> cercaUtente(String query, String campo){
+    }
+    
+    public List<Utente> visualizzaOrdinata(){
+    }
 }
