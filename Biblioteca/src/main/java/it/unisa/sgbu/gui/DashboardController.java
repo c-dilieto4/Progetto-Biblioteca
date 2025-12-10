a796935bf3cb2837b57c3bfffee511dc813cdc7e
dilie <dilie@host.docker.internal>
2025-12-10 10:14:34 +0100

    modify dashboard

--- Biblioteca/src/main/java/it/unisa/sgbu/gui/DashboardController.java
+++ Biblioteca/src/main/java/it/unisa/sgbu/gui/DashboardController.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package it.unisa.sgbu.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
